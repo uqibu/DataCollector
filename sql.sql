c058c434d079937ea72422cd0bc2ca3854c18db0
unknown <>
2016-12-25 00:36:24 +0800

    sql query1

--- sql.sql
+++ sql.sql
@@ -17,3 +17,21 @@ create table alluser(
   age bigint not null,
   addtime datetime not null default now()
 )default character set utf8;
+
+
+512247 | 笨蛋(剧场版)                  |   5258168 |
+512247 | 聆听世界                      |   5258156 |
+511932 | 唱响世界                      |   5252709 |
+511176 | 梦想的翅膀                    |   5239683 |
+510848 | 跨越新世界                    |   5234563 |
+24803  | 被风吹过的夏天                |    247579 |
+24803  | 不可思议                      |    247577 |
+24803  | 笨蛋                          |    247585 |
+10777  | 发现爱                        |    108576 |
+10770  | 期待爱                        |    108503 |
+
+SELECT artistsong.songName,artist.artistname,artistalbum.albumName,artistalbum.albumId FROM artistsong,artist,artistalbum
+where artistsong.artistId=artist.artistid
+and artistsong.artistId=artistalbum.artistId
+and artistsong.albumId=artistalbum.albumId
+and artistsong.artistId=7760 order by artistsong.albumId desc;
