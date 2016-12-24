fff37aa3b9800d570f01e61c5d55f9fb5d55778d
unknown <>
2016-12-25 00:04:03 +0800

    sql query

--- data.sql
+++ data.sql
@@ -1,4 +1,4 @@
-create table artist(
+﻿create table artist(
   artistid int not null primary key,
   artistname varchar(64) not null,
   aliasname varchar(64),
@@ -26,3 +26,29 @@ create table artistAlbum(
   artistId int not null,
   addtime datetime not null default now()
 )engine=innodb default character set utf8;
+
+select username,psw,gname,tel from (t1 left join t2 on t1.t1_id=t2.t1_id) left join t3 on t1.t1_id=t3.t1_id
+
+select * from a left join b on a.id=b.id left join c on b.id=c.id
+
+SELECT artistsong.songName,artist.artistname,artistalbum.albumName FROM artistsong 
+left join artist on artistsong.artistId=artist.artistid
+left join artistalbum on artistsong.artistId=artistalbum.artistId
+where artistsong.artistId=2116;
+
+SELECT artistsong.songName,artist.artistname,artistalbum.albumName FROM artistsong 
+left join artist on artistsong.artistId=artist.artistid
+left join artistalbum on artistsong.artistId=artistalbum.artistId 
+where artistsong.artistId=2116;
+
+SELECT artistsong.songName,artist.artistname,artistalbum.albumName FROM artistsong,artist,artistalbum
+where artistsong.artistId=artist.artistid
+and artistsong.artistId=artistalbum.artistId
+and artistsong.albumId=artistalbum.albumId
+and artistsong.artistId=8259 order by artistsong.albumId desc;
+
+SELECT artistsong.songName,artist.artistname,artistalbum.albumName FROM artistsong,artist,artistalbum
+where artistsong.artistId=artist.artistid
+and artistsong.artistId=artistalbum.artistId
+and artistsong.albumId=artistalbum.albumId
+and artistsong.artistId=8259 order by artistsong.albumId desc;
